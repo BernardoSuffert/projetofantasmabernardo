728007f389d034b93247ce5860c1e72967e3db7f
Bernardo Suffert <be@suffert.com>
2024-04-27 03:52:42 -0300

    grafico analise 1

--- rdocs/Bernardo.R
+++ rdocs/Bernardo.R
@@ -1,3 +1,67 @@
+banco<-read.csv("D:\\banco_final.csv")
+library(hms)
+library(lubridate)
 library(tidyverse)
-dadada<-c(1, 5)
-mean(dadada)
+décadas<- banco%>%
+  mutate(Ano=(year(date_aired)))
+decadas<- décadas%>%
+  select(Ano,format)
+decadas<- decadas%>%
+  mutate(frequencia=n())
+
+decadas<- decadas%>%
+  mutate(decada = case_when(
+    between(Ano, 1961, 1970) ~ "Década de 60",
+    between(Ano, 1971, 1980) ~ "Década de 70",
+    between(Ano, 1981, 1990) ~ "Década de 80",
+    between(Ano, 1991, 2000) ~ "Década de 90",
+    between(Ano, 2001, 2010) ~ "Década de 2000",
+    between(Ano, 2011, 2020) ~ "Década de 2010",
+    TRUE ~ "Década de 2020"
+  ))
+
+decadas%>%
+  group_by(decada,format)%>%
+  summarise(freq=n())
+
+ggplot(banco) +
+  aes(x = date_aired, y = format, group = format, colour = format) +
+  geom_line(size = 1) +
+  geom_point(size = 2) +
+  labs(x = "Ano", y = "Preço") +
+  estat_theme()
+
+trans_decadas <- decadas %>%
+  mutate(decada = case_when(
+    decada %>% str_detect("Década de 60") ~ "de 60",
+    decada %>% str_detect("Década de 70") ~ "de 70",
+    decada %>% str_detect("Década de 80") ~ "de 80",
+    decada %>% str_detect("Década de 90") ~ " de 90",
+    decada %>% str_detect("Década de 2000") ~ " de 2000",
+    decada %>% str_detect("Década de 2010") ~ " de 2010",
+    decada %>% str_detect("Década de 2020") ~ " de 2020"
+  )) %>%
+  group_by(decada, format) %>%
+  summarise(freq = n()) %>%
+  mutate(
+    freq_relativa = round(freq / sum(freq) * 100,1)
+  )
+
+porcentagens <- str_c(trans_decadas$freq_relativa, "%") %>% str_replace("\\.", ",")
+
+legendas <- str_squish(str_c(trans_decadas$freq, " (", porcentagens, ")"))
+
+ggplot(trans_decadas) +
+  aes(
+    x = fct_reorder(decada, freq, .desc = T), y = freq,
+    fill = format, label = legendas
+  ) +
+  geom_col(position = position_dodge2(preserve = "single", padding = 0)) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, hjust = 0.5,
+    size = 3
+  ) +
+  labs(x = "Décadas", y = "Frequência") +
+  estat_theme()
+ggsave("colunas-bi-freq.pdf", width = 158, height = 93, units = "mm")
